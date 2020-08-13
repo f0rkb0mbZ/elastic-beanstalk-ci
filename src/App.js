dd6a56079bbfedfd5c78de6b32b0cf22dfa3759f
Snehangshu Bhattacharya <snehangshubhattacharya@yahoo.in>
2020-08-13 23:45:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React App running on Docker!
+           A quick brown fox jumps over the lazy dog!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
